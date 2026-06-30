42f3cb6c332ff815d8a85c83b1bdb631ce47569e
RutvikPansare <rutvik.pansare@gmail.com>
2026-06-30 19:35:59 -0400

    T-195: make code editor margin consistent by adding top margin

--- src/ui/src/components/GraphQLWorkspace.tsx
+++ src/ui/src/components/GraphQLWorkspace.tsx
@@ -660,7 +660,7 @@ export function GraphQLWorkspace({ initialRequest }: GraphQLWorkspaceProps = {})
                       </div>
                     </div>
                   ) : (
-                    <div className="flex-1 min-h-0 flex flex-col p-2 pt-0">
+                    <div className="flex-1 min-h-0 flex flex-col p-2">
                       <div className="flex-1 min-h-0 rounded border border-[var(--border)] overflow-hidden">
                         <CodeMirror
                           value={query}
@@ -677,7 +677,7 @@ export function GraphQLWorkspace({ initialRequest }: GraphQLWorkspaceProps = {})
                 </div>
               </div>
             ) : bodyTab === 'variables' ? (
-              <div className="flex-1 min-h-0 p-2 pt-0">
+              <div className="flex-1 min-h-0 p-2">
                 <CodeMirror
                   value={variables}
                   height="100%"
